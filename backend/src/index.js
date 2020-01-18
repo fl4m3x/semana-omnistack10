19ded35f5de0c4c32b13cca5ef022c2ba4d3febe
DC WebSolutions <dcwebsolutions@MacBook-Pro-de-DC.local>
2020-01-17 23:50:16 -0300

    Semana Omnistack Finalizada

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb://omnistack10:omnistack10@ds125489.mlab.com:25489/omnistack10', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
